c235ff0ab14e20a52e1e84dcf80f7f591752b014
ericohwotu <eric.ohwotu@outlook.com>
2017-07-26 08:41:09 +0000

    Added Item Class

--- app/models/Item.scala
+++ app/models/Item.scala
@@ -3,23 +3,31 @@ package models
 import play.api.data._
 import play.api.data.Forms._
 
+import scala.collection.mutable.ListBuffer
+
 
 case class Item(name: String, price: Int, description: String = "",
                 manufacturer: String = "", warranty: Int = 720,
-                 discount: Int = 0, seller: String = "", image: String = "")
+                discount: Int = 0, seller: String = "", image: String = "")
 
-object Item{
+object Item {
 
-  val me = Form(
+  val itemForm: Form[Item] = Form(
     mapping(
       "name" -> nonEmptyText,
-      "price" -> number,
+      "price" -> number(strict = true),
       "description" -> text,
       "manufactureer" -> text,
       "warranty" -> number,
-      "discount" -> number(0,100),
+      "discount" -> number(0, 100),
       "seller" -> text,
       "image" -> text
-    )(Item.apply _)(Item.unapply _)
+    )(Item.apply _)(Item.unapply _))
+
+  val items: ListBuffer[Item] = ListBuffer(
+    Item("PS4", 245, "The ultimate in console gaming", "sony", 730),
+    Item("XBOX ONE", 245, "The ultimate in console gaming", "Microsoft", 730),
+    Item("Samsung Galaxy S8", 245, "The sexiest smartphone available", "Samsung", 730),
   )
+
 }
