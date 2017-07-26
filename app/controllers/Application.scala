0f4a49d8523368bf7ad92efa08dcb8eb090be3c6
ericohwotu <eric.ohwotu@outlook.com>
2017-07-26 08:57:40 +0000

    Added form handler

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,26 @@
 package controllers
 
-import play.api._
+import models.Item
+import javax.inject.Inject
+import play.api.i18n._
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport{
 
   //home page navbar helper
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def formHandler = Action { implicit request: Request[AnyContent] =>
+    val formResult = Item.itemForm.bindFromRequest
+
+    formResult.fold({
+      errors => BadRequest("bad")
+    },{
+      item => Item.items.append(item)
+        Ok("added")
+    })
   }
 
 }
